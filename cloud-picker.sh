fa467e0c761985d227ea1973b07484b0eafc32ab
Garrett Jansen <garrett@jansen.sh>
2018-08-21 19:44:07 -0400

    Some quick adjustments

--- cloud-picker.sh
+++ cloud-picker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -n "Enter the hostname and press [ENTER]: "
+echo "Enter the hostname and press [ENTER]: "
 read hostname 
-echo -n "Enter the root password and press [ENTER]: "
+echo "Enter the root password and press [ENTER]: "
 read rootpassword
 
 ansible-playbook generate_linode.yml --extra-vars "server_hostname=$hostname server_root_password=$password"
